b43dfd382b7eb786183756aa6d584c4311613a7c
JuanBarbosa <js.barbosa10@uniandes.edu.co>
2021-10-11 18:31:29 -0500

    added DOMs

---
+++ DOM/index.js
@@ -0,0 +1,22 @@
+/*
+    DOM
+*/
+
+// Select element by tag
+console.log(document.getElementsByTagName('div'));
+
+// Select element by ID
+console.log(document.getElementById('boldTitle'));
+
+// Select element by class
+console.log(document.getElementsByClassName('mainImage'));
+
+// Child nodes
+console.log(document.body.childNodes)
+
+// Query selector
+console.log(document.querySelector('div')); // para tags
+
+console.log(document.querySelector('#boldTitle')); // para IDs usar #
+
+console.log(document.querySelector('.mainImage')); // para clases usar .
